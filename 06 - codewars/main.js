1b421559a3446ba1a1aec9910828071a60f4393f
Adrián Zúñiga Pérez <105097849+4Driian@users.noreply.github.com>
2023-02-15 14:34:18 -0600

    Update Codewars

---
+++ 06 - codewars/main.js
@@ -0,0 +1,27 @@
+Number.prototype.add = function (n) {
+  return this + n;
+}
+
+Number.prototype.sub = function (n) {
+  return this - n;
+}
+
+Number.prototype.mul = function (n) {
+  return this * n;
+}
+
+Number.prototype.div = function (n) {
+  return this / n;
+}
+
+Number.prototype.pow = function (n) {
+  return Math.pow(this,n);
+}
+
+Number.prototype.sqr = function (n) {
+  return Math.sqrt(this,n);
+}
+
+/*------------------------------------------------*/
+
+
